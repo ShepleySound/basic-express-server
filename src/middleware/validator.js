2f1c368926e5fe0b9cde18facd9ed41fcc30ec0c
Robert Shepley <shepleysound@gmail.com>
2022-10-10 16:49:48 -0700

    Add next to validator.

--- src/middleware/validator.js
+++ src/middleware/validator.js
@@ -3,5 +3,5 @@
 module.exports = (req, res, next) => {
   if (req.query.name) {
     next();
-  } else throw new Error('Name required when looking for a person.');
+  } next('Name required when looking for a person.');
 };
